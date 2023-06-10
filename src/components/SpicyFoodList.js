6f12bb2fc1654744625dc564ac89981a9378eba7
Abdela Umer <adheremo65@gmail.com>
2023-06-10 16:45:36 -0400

    pro-hooks-state-arrays

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,21 +3,47 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [selected, setSelected] = useState("All");
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
-  }
+    const newFoodAdded = [...foods, newFood];
+    setFoods(newFoodAdded);
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+    // console.log(newFood);
+  }
+  function handleLiClick(id) {
+    const newFoodArray = foods.filter((food) => food.id !== id);
+    setFoods(newFoodArray);
+  }
+  const foodsToDisplay = foods.filter((food) => {
+    if (selected === "All") {
+      return true;
+    } else {
+      return food.cuisine === selected;
+    }
+  });
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
 
+  function handleSelect(e) {
+    setSelected(e.target.value);
+    console.log(e.target.value);
+  }
+
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
+      <select onChange={handleSelect} name="filter">
+        <option value={selected}>{selected}</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
       <ul>{foodList}</ul>
     </div>
   );
